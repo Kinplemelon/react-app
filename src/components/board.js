76fbc011cc54458ce55da3a3f3b7cffbfdfc3177
Kinplemelon <wujiu529@gmail.com>
2021-03-15 18:59:03 +0800

    Initially completed

--- src/components/board.js
+++ src/components/board.js
@@ -2,14 +2,41 @@ import React from "react";
 import Square from "./square.js";
 
 export default class Board extends React.Component {
-    renderSquare(i){
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            XIsNext: true,
+        };
+    }
+    handleClick(i) {
+        let squares = this.state.squares.slice();
+        squares[i] = this.state.XIsNext ? "X" : "O";
+        this.setState({
+            squares: squares,
+            XIsNext: !this.state.XIsNext,
+        });
+    }
+    renderSquare(i) {
         // 🔺🔺🔺 很特别的写法
         // 就是不是只有在render函数里面才能写JSX
         // 在普通函数里面也可以
-        return <Square value={i} />
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
     render() {
-        const status = 'Next player: X';
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = "Winner: " + winner;
+        } else {
+            // 🔺这个括号肥肠重要。
+            status = "Next player: " + (this.state.XIsNext ? "X" : "O");
+        }
         return (
             <div>
                 <div className="status">{status}</div>
@@ -29,6 +56,31 @@ export default class Board extends React.Component {
                     {this.renderSquare(8)}
                 </div>
             </div>
-        )
+        );
     }
+}
+
+// 不要在意这些细节。。。
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (
+            squares[a] &&
+            squares[a] === squares[b] &&
+            squares[a] === squares[c]
+        ) {
+            return squares[a];
+        }
+    }
+    return null;
 }
